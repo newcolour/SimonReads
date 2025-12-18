dca33c85aa88d81026f17130c1a39208c9275b43
Simone Bianco <simone@mac.lan>
2025-12-18 10:29:51 -0800

    Fix Android 15+/16 edge-to-edge status bar by disabling edge-to-edge mode in MainActivity

--- android/app/src/main/java/com/simonbianco/simonreads/MainActivity.java
+++ android/app/src/main/java/com/simonbianco/simonreads/MainActivity.java
@@ -1,5 +1,25 @@
 package com.simonbianco.simonreads;
 
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import androidx.core.view.WindowCompat;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Disable edge-to-edge mode on Android 15+ (API 35+) to allow
+        // StatusBar plugin to control the status bar background color
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
+            Window window = getWindow();
+            // Tell the system that we want to handle system bar insets ourselves
+            // This allows us to set a solid status bar background color
+            WindowCompat.setDecorFitsSystemWindows(window, true);
+        }
+    }
+}
