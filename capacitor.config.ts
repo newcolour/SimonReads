d80d938cfd5ce8569d2a1c74fd1997e7a9a1669a
Simone Bianco <simone@Simones-MacBook-Air.local>
2026-03-04 08:11:24 -0500

    Bump version to 4.5.7

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -9,7 +9,7 @@ const config: CapacitorConfig = {
   },
   android: {
     allowMixedContent: true,
-    captureInput: true,
+    captureInput: false,
     webContentsDebuggingEnabled: true
   },
   plugins: {
